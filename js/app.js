4b0bf873bc02a099db4199eaa2bc02d0c6622f8f
KanKan <cindykan@KanKan.local>
2018-01-15 21:40:42 -1000

    upto person14Species completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// THIS IS THE TEMPLATE CODE FOR XML
 // function reqListener () {
 //   console.log(this.responseText);
 // }
@@ -19,46 +20,67 @@ oReq.send();
 // declare function for new reqListener
 function reqListener() {
   // console.log(this);
-  console.log(this.responseText) // log response
+  console.log(this.responseText); // log response
   let person4Name = JSON.parse(this.responseText);
   document.getElementById('person4Name').innerText = person4Name.name;
 
   // must include second request in first function, as you need to fetch info first
   let oReq2 = new XMLHttpRequest();
   oReq2.addEventListener('load', reqListener2);
-  oReq2.open('GET', person4Name.homeworld); //person4Name.homeworld will lead to the OG link
+  oReq2.open('GET', person4Name.homeworld); //person4Name.homeworld will lead to the OG link, if sharing same link
   oReq2.send();
 }
 
 function reqListener2() {
-  console.log(this.responseText)
+  console.log(this.responseText);
   let person4HomeWorld = JSON.parse(this.responseText);
   document.getElementById('person4HomeWorld').innerText = person4HomeWorld.name;
 }
 
+// instantiate request for person14Name
+let oReq3 = new XMLHttpRequest();
+oReq3.addEventListener('load', reqListener3);
+oReq3.open('GET', 'https://swapi.co/api/people/14/');
+oReq3.send();
+
+function reqListener3() {
+  console.log(this.responseText);
+  let person14Name = JSON.parse(this.responseText);
+  document.getElementById('person14Name').innerText = person14Name.name;
+
+  let oReq4 = new XMLHttpRequest();
+  oReq4.addEventListener('load', reqListener4);
+  oReq4.open('GET', person14Name.species);
+  oReq4.send();
+}
+
+function reqListener4() {
+  console.log(this.responseText);
+  let person14Species = JSON.parse(this.responseText);
+  document.getElementById('person14Species').innerText = person14Species.name;
+
+}
+
+//get all films
+let oReq5 = new XMLHttpRequest();
+oReq5.addEventListener('load', reqListener5);
+oReq5.open('GET', 'https://swapi.co/api/films/');
+oReq5.send();
+
+function reqListener5() {
+  console.log(this.responseText);
+
+}
+
+let film = document.createElement('li');
+film.className = 'film';
+document.getElementById('filmList').appendChild(film);
+
+let filmTitle = document.createElement('h2');
+filmTitle.className = 'filmTitle';
+document.getElementById('film').appendChild(filmTitle);
+
+let filmPlanets = document.createElement('ul');
+filmPlanets.class
 
 
-// function reqListener() {
-//   console.log(this.responseText);
-
-//   let testName = JSON.parse(this.responseText); //converts from string
-//   document.getElementById('person4Name').innerText = testName.name;
-  
-//   var test2 = new XMLHttpRequest();
-//   test2.onload = reqListener;
-//   test.open('GET', testName.homeworld);
-//   test2.send();
-
-// }
-
-// //function getData() {
-//   var test = new XMLHttpRequest();
-//   test.onload = reqListener;
-//   test.open('GET', 'http://swapi.co/api/people/4/');
-//   test.send()
-// // }
-
-// function reqListener2 () {
-//   let testName2 = JSON.parse(this.responseText);
-//   document.getElementById('person4HomeWorld').innerText = testName2.name;
-// }
